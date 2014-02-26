f043560e69bef214203a1d6f4840784618973116
Sk Mohammadul Haque <mohammadul@gmail.com>
2014-02-26 11:07:39 +0530

    Update main.c

--- KinectCapture/src/main.c
+++ KinectCapture/src/main.c
@@ -878,7 +878,7 @@ static void show_about(GtkWidget *widget, gpointer data)
     gtk_about_dialog_set_name(GTK_ABOUT_DIALOG(dialog), "KinectCapture");
     gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(dialog), "0.2");
     gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(dialog),
-                                   "(Copyright (c) 2014 Sk. Mohammadul Haque");
+                                   "Copyright (c) 2014 Sk. Mohammadul Haque");
     gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(dialog),
                                   "KinectCapture is a software to view, capture raw Kinect data in different modes.");
     gtk_about_dialog_set_website(GTK_ABOUT_DIALOG(dialog), "http://mohammadulhaque.alotspace.com");
